f0c2fed26fc4d4c3ede5caaa5831fe4c02da6af8
Dmitry Pashkevich <dima-vanjuve@mail.ru>
2020-05-31 00:18:56 +0300

    init

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home page" />
+    <h1>Home page</h1>
+  </Layout>
+)
+
+export default IndexPage
